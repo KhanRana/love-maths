723f874436a26b7de15a9aba581fbbe05870f565
KhanRana <rana_khan@outlook.com>
2022-10-09 13:47:08 +0000

    add event listeners for document load and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
 
-function runGame(){
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You have clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
 
+})
 
-}
+function runGame() {
 
-function checkAnswer(){
-
-}
-
-function calculateCorrectAnswer(){
-
-}
-
-function incrementScore(){
-
-}
-
-function incrementWrongAnswer(){
-
-}
-
-function displayAdditionQuestion(){
 
 }
 
-function displaySubtractQuestion(){
+function checkAnswer() {
 
 }
 
-function displayMultiplyQuestion(){
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
 
 }
